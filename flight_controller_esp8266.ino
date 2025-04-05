ed0f744b48299ed5dca7c8729d1f0a289450733f
Hirokjyoti Kalita <55738876+hirokjyoti-k@users.noreply.github.com>
2025-04-05 15:40:04 +0530

    Update flight_controller_esp8266.ino

--- flight_controller_esp8266.ino
+++ flight_controller_esp8266.ino
@@ -1,99 +1,81 @@
 #include <Wire.h>
+#include <Adafruit_MPU6050.h>
+#include <Adafruit_Sensor.h>
 
-// MPU6050 Data Variables
-long accelX, accelY, accelZ;
-float gForceX, gForceY, gForceZ;
-long gyroX, gyroY, gyroZ;
-float rotX, rotY, rotZ;
+// MPU6050 object
+Adafruit_MPU6050 mpu;
 
-// ESC PWM output pins for NodeMCU (D5, D6, D7, D8)
+// ESC pins for NodeMCU
 #define ESC1 D5
 #define ESC2 D6
 #define ESC3 D7
 #define ESC4 D8
 
-int throttle = 1200;  // Base throttle (PWM range: 1000-2000)
-
-// Setup MPU6050
-void setupMPU() {
-  Wire.beginTransmission(0x68);
-  Wire.write(0x6B); Wire.write(0x00); Wire.endTransmission();
-  Wire.beginTransmission(0x68);
-  Wire.write(0x1B); Wire.write(0x00); Wire.endTransmission();
-  Wire.beginTransmission(0x68);
-  Wire.write(0x1C); Wire.write(0x00); Wire.endTransmission();
-}
-
-// Read Accelerometer Data
-void recordAccelRegisters() {
-  Wire.beginTransmission(0x68);
-  Wire.write(0x3B); Wire.endTransmission();
-  Wire.requestFrom(0x68, 6);
-  while (Wire.available() < 6);
-  accelX = Wire.read() << 8 | Wire.read();
-  accelY = Wire.read() << 8 | Wire.read();
-  accelZ = Wire.read() << 8 | Wire.read();
-  gForceX = accelX / 16384.0;
-  gForceY = accelY / 16384.0;
-  gForceZ = accelZ / 16384.0;
-}
-
-// Read Gyroscope Data
-void recordGyroRegisters() {
-  Wire.beginTransmission(0x68);
-  Wire.write(0x43); Wire.endTransmission();
-  Wire.requestFrom(0x68, 6);
-  while (Wire.available() < 6);
-  gyroX = Wire.read() << 8 | Wire.read();
-  gyroY = Wire.read() << 8 | Wire.read();
-  gyroZ = Wire.read() << 8 | Wire.read();
-  rotX = gyroX / 131.0;
-  rotY = gyroY / 131.0;
-  rotZ = gyroZ / 131.0;
-}
+int throttle = 1200; // base throttle (1000-2000)
 
 void setup() {
   Serial.begin(115200);
-  Wire.begin(D2, D1); // SDA = D2, SCL = D1
+  Wire.begin(D2, D1); // SDA = D2, SCL = D1 (NodeMCU)
 
+  // Setup ESC pins
   pinMode(ESC1, OUTPUT);
   pinMode(ESC2, OUTPUT);
   pinMode(ESC3, OUTPUT);
   pinMode(ESC4, OUTPUT);
 
-  setupMPU();
+  // Initialize MPU6050
+  if (!mpu.begin()) {
+    Serial.println("MPU6050 not found!");
+    while (1) delay(10);
+  }
+  Serial.println("MPU6050 initialized!");
 
-  // ESC Calibration: Send minimum throttle
-  analogWrite(ESC1, map(1000, 1000, 2000, 0, 1023));
-  analogWrite(ESC2, map(1000, 1000, 2000, 0, 1023));
-  analogWrite(ESC3, map(1000, 1000, 2000, 0, 1023));
-  analogWrite(ESC4, map(1000, 1000, 2000, 0, 1023));
-  delay(2000);
+  // Configurations
+  mpu.setAccelerometerRange(MPU6050_RANGE_8_G);
+  mpu.setGyroRange(MPU6050_RANGE_500_DEG);
+  mpu.setFilterBandwidth(MPU6050_BAND_5_HZ);
+
+  delay(100);
+
+  // ESC calibration (send min throttle)
+  for (int i = 0; i < 4; i++) {
+    analogWrite(ESC1, map(1000, 1000, 2000, 0, 1023));
+    analogWrite(ESC2, map(1000, 1000, 2000, 0, 1023));
+    analogWrite(ESC3, map(1000, 1000, 2000, 0, 1023));
+    analogWrite(ESC4, map(1000, 1000, 2000, 0, 1023));
+    delay(500);
+  }
 }
 
 void loop() {
-  recordAccelRegisters();
-  recordGyroRegisters();
+  // Get sensor events
+  sensors_event_t accel, gyro, temp;
+  mpu.getEvent(&accel, &gyro, &temp);
 
-  // Basic Flight Stabilization
+  // Use gyroscope data (rotation) for basic stabilization
+  float rotX = gyro.gyro.x * 57.2958; // convert rad/s to deg/s
+  float rotY = gyro.gyro.y * 57.2958;
+  float rotZ = gyro.gyro.z * 57.2958;
+
+  // Basic flight stabilization math
   int esc1_speed = throttle + rotX - rotY + rotZ;
   int esc2_speed = throttle - rotX - rotY - rotZ;
   int esc3_speed = throttle - rotX + rotY + rotZ;
   int esc4_speed = throttle + rotX + rotY - rotZ;
 
-  // Limit motor speeds between 1000-2000 µs
+  // Clamp values to 1000-2000
   esc1_speed = constrain(esc1_speed, 1000, 2000);
   esc2_speed = constrain(esc2_speed, 1000, 2000);
   esc3_speed = constrain(esc3_speed, 1000, 2000);
   esc4_speed = constrain(esc4_speed, 1000, 2000);
 
-  // Convert to ESP8266 PWM scale (0-1023)
+  // Convert to 0-1023 for ESP8266 PWM
   analogWrite(ESC1, map(esc1_speed, 1000, 2000, 0, 1023));
   analogWrite(ESC2, map(esc2_speed, 1000, 2000, 0, 1023));
   analogWrite(ESC3, map(esc3_speed, 1000, 2000, 0, 1023));
   analogWrite(ESC4, map(esc4_speed, 1000, 2000, 0, 1023));
 
-  // Debugging
+  // Debug output
   Serial.print("Roll: "); Serial.print(rotX);
   Serial.print(" | Pitch: "); Serial.print(rotY);
   Serial.print(" | Yaw: "); Serial.print(rotZ);
@@ -102,5 +84,5 @@ void loop() {
   Serial.print(" | ESC3: "); Serial.print(esc3_speed);
   Serial.print(" | ESC4: "); Serial.println(esc4_speed);
 
-  delay(20);  // 50Hz update rate
+  delay(20); // 50 Hz update rate
 }
